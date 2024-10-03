64dac858f208ac76f3f2098afdb4bff3489fda6e
KandieDaisy <kandiedaisy353@gmail.com>
2024-10-03 16:03:01 +0300

    Update for loop .cpp

---
+++ for loop .cpp
@@ -0,0 +1,17 @@
+/*Author: daisy kandie 
+Reg: BSE-01-0039/2024
+Description:For loop
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, sum = 0;
+    for(i = 1; i <= 100; i++) {
+        cout << i << endl;
+        sum = sum + i;  // Add i to sum
+    }
+    cout << "The sum is " << sum << endl;
+
+    return 0;
+}
