2a088d8f8745f5662cc2daeb21875cf0df602f70
KandieDaisy <kandiedaisy353@gmail.com>
2024-10-03 15:57:39 +0300

    assigments

---
+++ do while loop.cpp
@@ -0,0 +1,19 @@
+/*Author:daisy kandie
+Reg:BSE-01-0039/2024
+Description: do while loop
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1, sum = 0;
+    do {
+        sum = sum + i;  // Add i to sum first
+        cout << i << endl;
+        i++;  
+    } while(i > 0 && i <= 100);  
+    
+    cout << "The sum is " << sum << endl;
+
+    return 0;
+}
